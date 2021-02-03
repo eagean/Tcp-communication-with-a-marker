cf080300016530ee2b8c69694018e9789abe4a0d
Ege Özfırıncı <ozfrnce@users.noreply.github.com>
2021-02-03 04:23:25 +0300

    Source codes and report

---
+++ TcpCli.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpClient
+{
+    class TcpCli
+    {
+        static void Main(string[] args)
+        {
+            string recievedtosv;
+            string input;
+            string markedinput;
+            string endmarker = "<##!##>";
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork,
+                                       SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Unable to connect to server.");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+            NetworkStream stream = new NetworkStream(server);
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+            recievedtosv = reader.ReadLine();
+            Console.WriteLine(recievedtosv);
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                markedinput = input +" "+ endmarker;
+
+                writer.WriteLine(markedinput);
+                writer.Flush();
+               
+                recievedtosv = reader.ReadLine();
+                Console.WriteLine(recievedtosv);
+            }
+            Console.WriteLine("Disconnecting from server...");
+            reader.Close();
+            writer.Close();
+            stream.Close();
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
+
+    }
+}
---
+++ TcpSv.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpServer
+{
+    class TcpSv
+    {
+        static void Main(string[] args)
+        {
+            string recieveddata;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            string endmarker = "<##!##>";
+            ArrayList msg = new ArrayList();
+
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket newsock = new Socket(AddressFamily.InterNetwork,
+                                         SocketType.Stream, ProtocolType.Tcp);
+            newsock.Bind(ipep);
+            newsock.Listen(10);
+            Console.WriteLine("Waiting for a client...");
+            Socket client = newsock.Accept();
+            IPEndPoint newclient = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Connected with {0} at port {1}",
+                                 newclient.Address, newclient.Port);
+            NetworkStream ns = new NetworkStream(client);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            string welcome = "Welcome to ege's server";
+            sw.WriteLine(welcome);
+            sw.Flush();
+            while (true)
+            {
+                recieveddata = sr.ReadLine();
+                string[] msgdata = recieveddata.Split(delimiterChars);
+
+                foreach (string ege in msgdata)
+                {
+
+                    if (string.Equals(ege, endmarker))
+                    {
+                        Console.WriteLine("message end ");
+
+                    }
+                    else
+                    {
+                        msg.Add(ege);
+                    }
+
+                }
+                string realmsg = string.Join(" ", msg.ToArray());
+                Console.WriteLine(realmsg);
+                sw.WriteLine(realmsg);
+                sw.Flush();
+                msg.Clear();
+            }           
+            Console.WriteLine("Disconnected from {0}", newclient.Address);
+            sw.Close();
+            sr.Close();
+            ns.Close();
+        }
+     }
+
+  }
+
+
+
